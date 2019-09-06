8b82fa248f96ab1324f66892c2256fae87319984
Adam <adam.vichik@gmail.com>
2019-09-06 02:19:04 -0700

    Create methods to update the PID variables

--- src/main/java/robot/AdamPID.java
+++ src/main/java/robot/AdamPID.java
@@ -1,4 +1,4 @@
-package com.company;
+package robot;
 
 public class AdamPID {
     AdamPID adampid = new AdamPID(1, 1, 1, 1);
@@ -10,6 +10,8 @@ public class AdamPID {
     public double Integral = 0;
     public double lastError = error;
     public double derivative;
+    public double setpoint;
+    public double distance;
     public AdamPID (double input_kP, double input_kI, double input_kD, double input_LoopTime) {
         kP = input_kP;
         kI = input_kI;
@@ -17,29 +19,34 @@ public class AdamPID {
         LoopTime = input_LoopTime;
 
     }
-    public void UpdateKP() {
 
+    public void UpdateKP(double kP) {
+        this.kP = kP;
     }
-    public void UpdateKI() {
-
+    public void UpdateKI(double kI) {
+        this.kI = kI;
     }
-    public void UpdateKD() {
-
+    public void UpdateKD(double kD) {
+        this.kD = kD;
     }
-    public void setSetpoint(double ) {
-
+    public void setSetpoint(double targetSetpoint) {
+       setpoint = targetSetpoint;
     }
-    public void update(double ) {
-        error = targetDistance - driveSubsystem.getDistance(); //proportional
+    public void update(double distance) {
+        error = setpoint - distance; //proportional
         Integral += error * LoopTime;
         derivative = (error - lastError) / LoopTime;
-        adampid.setSpeed(kP * error + kI * Integral + kD * derivative); //PID
+        adampid.getOutput(); //PID
         lastError = error;
+        UpdateKD(kP);
+        UpdateKI(kI);
+        UpdateKP(kP);
     }
     public double getOutput() {
-
+        return kP * error + kI * Integral + kD * derivative;
     }
     public void reset(){
-
+        Integral = 0;
+        lastError = error;
     }
 }
